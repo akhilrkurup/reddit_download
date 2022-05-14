a88d93fea6d26537b833f979c0fe82d50fc21ddc
akhilrkurup <89651499+akhilrkurup@users.noreply.github.com>
2022-05-14 21:53:12 +0530

    first

--- reddit.py
+++ reddit.py
@@ -4,3 +4,15 @@ Created on Sat May 14 20:08:29 2022
 
 @author:Akhil R Kurup
 """
+import praw, json
+
+reddit = praw.Reddit(
+    client_id="mlCV4KaLVVmrOj6nz2ULZQ",
+    client_secret="DC0wvImgiUPixsQR2FrJvkvdr1JOew",
+    user_agent="my user agent",
+)
+
+sr = reddit.subreddit("memes")
+for subm in sr.hot(limit=1):
+    with open("vars.txt", "w") as f:
+        f.write(str(vars(subm)))
