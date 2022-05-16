d99e206693d154d1c51c5decd2f265895e27d251
akhilrkurup <89651499+akhilrkurup@users.noreply.github.com>
2022-05-16 11:18:44 +0530

    fixed

--- write_url.py
+++ write_url.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 14 20:08:29 2022
+Created on Mon May 16 11:06:59 2022
 
 @author:Akhil R Kurup
 """
-import praw
-import requests
+import praw, pickle
 
 reddit = praw.Reddit(
     client_id="mlCV4KaLVVmrOj6nz2ULZQ",
@@ -14,15 +13,13 @@ reddit = praw.Reddit(
 )
 
 sr = reddit.subreddit("whenthe")
-python_subr = sr.top(limit=50)
+python_subr = sr.top(limit=25)
 urls = []
 titles = []
 for submission in python_subr:
     urls.append(submission.url)
     titles.append(submission.title)
-res = requests.get(urls[0])
-print(res.status_code)
-with open(titles[0] + ".gif", "wb") as out:
-    out.write(res.content)
-with open("counter.txt", "w") as counter_txt:
-    counter_txt.write(i)
+with open("urls.pickle", "wb") as f:
+    pickle.dump(urls, f, pickle.HIGHEST_PROTOCOL)
+with open("titles.pickle", "wb") as g:
+    pickle.dump(titles, g, pickle.HIGHEST_PROTOCOL)
